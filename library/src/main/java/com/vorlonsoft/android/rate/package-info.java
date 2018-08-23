3a6c65dd6703b69eceeed7c21fef4b020cf769dc
Alexander Savin <info@vorlonsoft.com>
2018-08-23 05:24:17 +0300

    LIBff10823 package-info.java added

---
+++ library/src/main/java/com/vorlonsoft/android/rate/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2017 - 2018 Vorlonsoft LLC
+ *
+ * Licensed under The MIT License (MIT)
+ */
+
+/**
+ * <p>AndroidRate Library</p>
+ * <p>Library to help you promote your Android app by prompting
+ * users to rate the app after using it for a few days.</p>
+ *
+ * @since    0.0.4
+ * @version  1.1.9
+ * @author   Alexander Savin
+ * @author   Shintaro Katafuchi
+ */
+package com.vorlonsoft.android.rate;
