18b7bdf15074ae517aa1a95a6df9022b7122778a
Alexander Savin <info@vorlonsoft.com>
2018-10-15 02:02:21 +0300

    LIBff21015 Convert DialogType.java to DialogType.kt

---
+++ library/src/main/java/com/vorlonsoft/android/rate/DialogType.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018 Vorlonsoft LLC
+ *
+ * Licensed under The MIT License (MIT)
+ */
+
+package com.vorlonsoft.android.rate
+
+import androidx.annotation.IntDef
+import com.vorlonsoft.android.rate.Constants.Utils.UTILITY_CLASS_MESSAGE
+
+/**
+ * DialogType Class - the dialog types class of the AndroidRate library.
+ *
+ * @constructor DialogType is an utility class and it can't be instantiated.
+ * @since       1.2.1
+ * @version     1.2.1
+ * @author      Alexander Savin
+ */
+class DialogType private constructor() {
+    /** DialogType Class initializer block. */
+    init {
+        throw UnsupportedOperationException("DialogType$UTILITY_CLASS_MESSAGE")
+    }
+
+    /**
+     * Denotes that the annotated element of the primitive type int represents a logical type and
+     * that its value should be one of the following constants: [APPLE], [CLASSIC], [MODERN].
+     *
+     * @since       1.2.1
+     * @version     1.2.1
+     * @author      Alexander Savin
+     */
+    @MustBeDocumented
+    @Retention(AnnotationRetention.SOURCE)
+    @IntDef(APPLE, CLASSIC, MODERN)
+    annotation class AnyDialogType
+
+    /** The singleton contains constants for dialog types. */
+    companion object {
+        /** Apple Rate Dialog. */
+        const val APPLE = 0
+        /** Classic Rate Dialog. */
+        const val CLASSIC = 1
+        /** Modern Rate Dialog. */
+        const val MODERN = 2
+    }
+}
