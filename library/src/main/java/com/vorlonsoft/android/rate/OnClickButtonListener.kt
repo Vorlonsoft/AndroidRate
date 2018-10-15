b42dff4b1b1f8233430f85c60ec7781716bb151a
Alexander Savin <info@vorlonsoft.com>
2018-10-15 05:18:13 +0300

    LIBff41015 Convert OnClickButtonListener.java to OnClickButtonListener.kt

---
+++ library/src/main/java/com/vorlonsoft/android/rate/OnClickButtonListener.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2017 - 2018 Vorlonsoft LLC
+ *
+ * Licensed under The MIT License (MIT)
+ */
+
+package com.vorlonsoft.android.rate
+
+/**
+ * OnClickButtonListener Interface - the Rate Dialog buttons on-click listener interface of the
+ * AndroidRate library.
+ *
+ * You can implement it and use `AppRate.with(this).setOnClickButtonListener(OnClickButtonListener)`
+ * to specify a callback when the Rate Dialog button is pressed. The same value as the second
+ * argument of [android.content.DialogInterface.OnClickListener.onClick] will be passed in the
+ * argument of [OnClickButtonListener.onClickButton].
+ *
+ * @since    0.2.0
+ * @version  1.2.1
+ * @author   Alexander Savin
+ * @author   Shintaro Katafuchi
+ */
+interface OnClickButtonListener {
+
+    /**
+     * A callback when the Rate Dialog button is pressed.
+     *
+     * @param which the same value as the second argument of
+     * [android.content.DialogInterface.OnClickListener.onClick].
+     */
+    fun onClickButton(which: Byte)
+}
