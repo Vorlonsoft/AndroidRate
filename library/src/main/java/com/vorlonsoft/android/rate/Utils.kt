1a08351dd903cf028807255ee32077c414e0cffb
Alexander Savin <info@vorlonsoft.com>
2018-10-25 00:01:23 +0300

    LIBcc11024 Backing property _isLollipop implemented

--- library/src/main/java/com/vorlonsoft/android/rate/Utils.kt
+++ library/src/main/java/com/vorlonsoft/android/rate/Utils.kt
@@ -32,8 +32,16 @@ import com.vorlonsoft.android.rate.Constants.Utils.EMPTY_STRING_ARRAY
 internal object Utils {
     /** Whether the Android Lollipop (API 21-22) is installed on the user device or not. */
     @JvmStatic
+    private var _isLollipop: Boolean? = null
+    /** Whether the Android Lollipop (API 21-22) is installed on the user device or not. */
+    @JvmStatic
     val isLollipop: Boolean
-        get() = ((SDK_INT == LOLLIPOP) || (SDK_INT == LOLLIPOP_MR1))
+        get() {
+            if (_isLollipop == null) {
+                _isLollipop = ((SDK_INT == LOLLIPOP) || (SDK_INT == LOLLIPOP_MR1))
+            }
+            return _isLollipop ?: false
+        }
 
     /**
      * Creates [android.app.AlertDialog.Builder].
