3573bcae29ca891831a58255f7446985547c29ec
d.wagner <d.wagner@stashcat.com>
2023-02-08 17:49:02 +0100

    :arrow_up: Update Gradle and Versions

--- sample/src/main/java/com/vorlonsoft/android/rate/sample/SampleApplication.java
+++ sample/src/main/java/com/vorlonsoft/android/rate/sample/SampleApplication.java
@@ -7,36 +7,10 @@
 package com.vorlonsoft.android.rate.sample;
 
 import android.app.Application;
-import android.os.Process;
 
-import com.squareup.leakcanary.LeakCanary;
-
-@SuppressWarnings("WeakerAccess")
 public class SampleApplication extends Application {
 
     public SampleApplication() {
         super();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        //noinspection ConstantConditions
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        //noinspection ResultOfMethodCallIgnored
-        LeakCanary.install(this);
-        // Normal app init code...
-    }
-
-    @Override
-    public void onTerminate() {
-        System.exit(0);
-        Process.killProcess(Process.myPid());
-
-        super.onTerminate();
     }
 }
